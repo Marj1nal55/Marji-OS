6f5a81846d281f9a2a864e9a85bb2781149bf17d
Marj1nal55 <akkusmirac055@gmail.com>
2026-09-09 01:39:56 +0300

    Working shell: fork, execvp, wait - runs real Linux commands

--- shell/shell.c
+++ shell/shell.c
@@ -1,14 +1,48 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
 int main()
 {
    char intput[100];
+   char *args[10];
 
    while(1)
    {
-    printf("$:");
-    fgets(intput, sizeof(intput), stdin);
-    printf("%s\n", intput);
-   }
-   return 0;
+      int i = 0;
+
+      printf("$:");
+      fgets(intput, sizeof(intput), stdin);
+      intput[strcspn(intput, "\n")] = '\0';
+      char *parca = strtok(intput, " ");
+      while(parca != NULL)
+      {
+        args[i] = parca;
+        i++;
+        parca = strtok(NULL, " ");
+      }
+      args[i] = NULL;
+
+      pid_t pid = fork();
+      if(pid == 0)
+      {
+        // buradayım = ben child'ım
+        execvp(args[0], args);
+        perror("execvp basarisiz");
+        exit(1);
+      }
+      else if  (pid > 0)
+      {
+        // buradayım = ben parent'ım
+        wait(NULL);  // çocuğun bitmesini bekle
+      }
+      else
+      {
+        printf("fork basarisiz\n");
+      }
+      printf("%s\n", intput);
+    }
+    return 0;
 }
