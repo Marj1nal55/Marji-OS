e0f0ae34ae7cbccca590c30f16c53efc43211e5a
Marj1nal55 <akkusmirac055@gmail.com>
2026-09-09 10:53:50 +0300

    Parse pipe syntax: split into two commands before parsing args

--- shell/shell.c
+++ shell/shell.c
@@ -4,26 +4,38 @@
 #include<unistd.h>
 #include<sys/wait.h>
 
+void parse_command(char *command, char *args[])
+{
+   int i = 0;
+   char *parca = strtok(command, " ");
+   while (parca != NULL) {
+        args[i] = parca;
+        i++;
+        parca = strtok(NULL, " ");
+   }
+   args[i] = NULL;
+}
+
 int main()
 {
    char intput[100];
    char *args[10];
+   char *args2[10];
    while(1)
    {
-      int i = 0;
 
       printf("$:");
       fgets(intput, sizeof(intput), stdin);
       intput[strcspn(intput, "\n")] = '\0';
-      char *parca = strtok(intput, " ");
-      while(parca != NULL)
-      {
-        args[i] = parca;
-        i++;
-        parca = strtok(NULL, " ");
-      }
-      args[i] = NULL;
+      char *komut1 = strtok(intput, "|");
+      char *komut2 = strtok(NULL, "|");
+      parse_command(komut1, args);
+      if (komut2 != NULL) {
+        parse_command(komut2, args2);
+       }
 
+      printf("args[0]=%s\n", args[0]);
+      if (komut2 != NULL) printf("args2[0]=%s\n", args2[0]);
       if(strcmp(args[0], "exit") == 0)
       {
         exit(0);
